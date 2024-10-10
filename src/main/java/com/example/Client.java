b515ed7761f73ea4b3e3223990ae76c4345f5b1c
Kevin N. Mathisen <kevin.nikolai.mathisen@gmail.com>
2024-10-10 18:47:16 +0200

    Add comments in Client.java

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -27,16 +27,27 @@ public class Client {
     private final Listener spread_listener;             // Used to receive messages
     private final SpreadGroup spread_group;             // Group for account
 
-    private final int id;
+    private final int id;                           // Unique ID of the client
     private boolean client_syncronising;            // If the client is currently syncronising
 
-    private final String file_name;
-    private final boolean naive_sync_balance;
-    public SpreadGroup[] members;                  // TODO: implement updated members list
+    private final String file_name;                 // File name to get commands from, empty '' when we read from command line
+    private final boolean naive_sync_balance;       // If naive mode should be used when running the command getSyncedBalance
+    public SpreadGroup[] members;                   // The current members in the group
 
+                                                    // Scheduler for broadcasting every 10 seconds
     private final ScheduledExecutorService schedulerBroadcast = Executors.newScheduledThreadPool(1);
 
-    public Client(int id, String server_address, String account_name, String file_name, int num_of_replica, boolean naive_sync_balance) throws InterruptedException {
+    /**
+     * Constructor for Client, which intializes variables and tries to connect the client to the spread server
+     * 
+     * @param id                    - Unique id of client
+     * @param server_address        - Ip address of spread server
+     * @param account_name          - Group name in spread server
+     * @param file_name             - File to get commands from, empty if from command line
+     * @param naive_sync_balance    - If naive mode should be used when running the command getSyncedBalance 
+     * @throws InterruptedException
+     */
+    public Client(int id, String server_address, String account_name, String file_name, boolean naive_sync_balance) throws InterruptedException {
         // Initialize to 0 and empty lists
         this.balance = 0.0;
         this.order_counter = 0;
@@ -49,18 +60,20 @@ public class Client {
         this.spread_listener = new Listener(this);
 
         this.id = id;
-        this.client_syncronising = false;   
+        this.client_syncronising = false;   // Start all clients in normal mode
 
         this.file_name = file_name;
         this.naive_sync_balance = naive_sync_balance;
 
-        // Connect to spread server, connect the listener, and join the spread group
         try {
-            spread_connection.add(spread_listener);
-            spread_connection.connect(InetAddress.getByName(server_address), 4803, String.valueOf(id), false, true);
+            spread_connection.add(spread_listener);         // Add the listener
+            spread_connection.connect(                      // Connect to the spread server
+                InetAddress.getByName(server_address), 4803, String.valueOf(id), false, true
+            );
 
-            this.spread_group = new SpreadGroup();
+            this.spread_group = new SpreadGroup();          // Join the spread group for our account
             this.spread_group.join(spread_connection, account_name);
+
         } catch (SpreadException e) {
             throw new RuntimeException(e);
         } catch (UnknownHostException e) {
@@ -68,16 +81,24 @@ public class Client {
         }
     }
 
+    /**
+     * Read arguments, creates a client, waits for all other clients to join, starts broadcasting the outstanding_colleciton, and then reads commands
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
+        // Temporary command line argument
+        // TODO: read arguments from command line
         String server_address = "127.0.0.1";
         String account_name = "Group10";
         String file_name = "";
         int num_of_replica = 1;
         boolean naive_sync_balance = false;
 
+        // Initialize client
         Random rand = new Random();
         int client_id = rand.nextInt(Integer.MAX_VALUE)+1;
-        Client client = new Client(client_id, server_address, account_name, file_name, num_of_replica, naive_sync_balance);
+        Client client = new Client(client_id, server_address, account_name, file_name, naive_sync_balance);
 
         System.out.println("Created client with ID " + client_id + ", waiting for " + num_of_replica + " other clients to join group");
         
@@ -102,6 +123,11 @@ public class Client {
         Thread.sleep(100000000);
     }
 
+    /**
+     * Takes in a command, checks if the command has valid syntax, and then runs code to perform the command
+     * @param command - The command to execute
+     * @throws Exception
+     */
     private void processCommand(String command) throws Exception {
         command = command.trim();
 
@@ -138,6 +164,8 @@ public class Client {
         if (command.startsWith("getSynchedBalance")) {
             if (naive_sync_balance) getSyncedBalanceNaive();
             else                    getSyncedBalanceCorrect();
+
+            return;
         }
 
         // Simply call the methods of the other commands which require no arguments
@@ -152,6 +180,9 @@ public class Client {
 
     }
 
+    /**
+     * Broadcasts outstanding_collection every 10 seconds if the client is not syncronising
+     */
     private void broadcastOutstanding() {
         // calls sendOutstanding every 10 seconds
         schedulerBroadcast.scheduleAtFixedRate(() -> {
@@ -164,21 +195,40 @@ public class Client {
         }, 10, 10, TimeUnit.SECONDS);
     }
 
+    /**
+     * Sends outstanding_collection to the spread group
+     * @throws InterruptedException
+     */
     private void sendOutstanding() throws InterruptedException {
         try {
+            // Create spread message, add it to our account, and make it follow FIFO to ensure consistent view
             SpreadMessage message = new SpreadMessage();
             message.addGroup(spread_group);
             message.setFifo();
+
+            // Set the message content to be the outstanding_collection
             message.setObject((Serializable) outstanding_collection);
+
+            // Send the message to the other members of the group
             spread_connection.multicast(message);
-            // Outstanding collection is broadcast, but not cleared, which could in some cases cause transactions to be broadcast twice
-            //   However this should not a problem, as clients receiving transactions should check if they have already been executed
+
+            // Note:
+            //      Outstanding collection is broadcast, but not cleared, which could in some cases cause transactions to be broadcast twice
+            //      However this should not a problem, as clients receiving transactions should check if they have already been executed
 
         } catch (SpreadException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Takes in transactions received from the spread server, and executed them if no problems are detected
+     * Handles getSyncedBalance, setBalance, deposit, and addInterest
+     * When a deposit or addInterest transaction is executed the order_counter is incremented and the transaction is saved to executed_list 
+     * Also notifies getSynchedBalance if outstanding_collection is empty when it is implemented as naive
+     * 
+     * @param receivedTransactions - A collection containing transactions reveived from spread, which is executed in order of collection (which should be the same for all replicas??)
+     */
     public void processReceivedTransactions(Collection<Transaction> receivedTransactions) {
         synchronized (this) {
             // Iterate over all transactions received
@@ -215,13 +265,13 @@ public class Client {
                     continue;
                 }
 
-                // Check if setBalance
+                // If setbalance command for syncronisation, send it to handleSetBalance(...)
                 if (commandParts[0].equals("setBalance")) {
                     handleSetBalance(Double.parseDouble(commandParts[1]));
                     continue;
                 }
 
-                // Execute commands by using their method, skip transaction if it is not deposit or addInterest
+                // Execute deposit and addInterest commands by using their method, skip transaction if invalid transaction type
                 switch (commandParts[0]) {
                     case "deposit" -> deposit(Double.parseDouble(commandParts[1]));
                     case "addInterest" -> addInterest(Double.parseDouble(commandParts[1]));
@@ -230,17 +280,22 @@ public class Client {
                     }
                 }
 
-                // Count transaction and save it to history
+                // Count deposit and addInterest transaction and save it to history
                 order_counter++;
                 executed_list.add(tx);
             }
 
+            // If outstanding_collection empty and are using navie sync balance, notify potential getSyncedBalance waiting for outstanding_collection to be empty
             synchronized (outstanding_collection) {
-                if (outstanding_collection.isEmpty()) outstanding_collection.notifyAll();
+                if (outstanding_collection.isEmpty() && naive_sync_balance) outstanding_collection.notifyAll();
             }
         }
     }
 
+    /**
+     * 
+     * @param members
+     */
     public void handleMemberShipChange(SpreadGroup[] members) {
         // If a member leaves the exection, update member list but otherwise ignore it.
 
@@ -257,6 +312,10 @@ public class Client {
         //      If everything ok,  we can resume execution and broadcasting
     }
 
+    /**
+     * 
+     * @param balance
+     */
     private void handleSetBalance(double balance) {
         // TODO: handle what happens if setbalance
 
@@ -274,12 +333,18 @@ public class Client {
         //      if this is equal to members size - 1, we have received all setBalance, and should continue executing by setting sync mode to false
     }
 
+    /**
+     * Prints the balance immediately
+     */
     private void getQuickBalance() {
         System.out.println("Quick balance: " + balance);
     }
 
+    /**
+     * Print the balance when outstanding collection is empty
+     */
     private void getSyncedBalanceNaive() {
-        // wait in another thread for outstanding_collection to be empty
+        // wait in a thread for outstanding_collection to be empty
         //   then print balance
         new Thread(() -> {
             synchronized (outstanding_collection) {
@@ -295,6 +360,9 @@ public class Client {
         }).start();
     }
 
+    /**
+     * Print the balance by adding a transaction which will print the balance when it is later broadcasted and executed
+     */
     private void getSyncedBalanceCorrect() {
         // create a transaction 
         Transaction tx = new Transaction();
@@ -307,14 +375,26 @@ public class Client {
         }
 }
 
+    /**
+     * Add given amount to the balance
+     * @param amount - How much to add to the balance. Can be negative
+     */
     private void deposit(double amount) {
         balance += amount;
     }
 
+    /**
+     * Apply interest to the balance
+     * @param percent - How much to interest (in percent) to add to the balance. Can be negative
+     */
     private void addInterest(double percent) {
         balance += balance * (percent / 100.0);
     }
     
+    /**
+     * Print transactions from the executed list and the outstanding_collection
+     * The transactions in executed_list are numbered based on order_counter
+     */
     private void getHistory() {
         // TODO: should print out history
         //   transactions from executed list should be numbered from (order_counter-executed_list.length()) and up
@@ -322,34 +402,57 @@ public class Client {
         System.out.println("History: ");
     }
 
+    /**
+     * Print the status of the transaction with the ID given
+     * @param transactionId - The id of the transaction to find status of
+     */
     private void printTxStatus(String transactionId) {
         System.err.println("Transaction with ID " + transactionId + " has status: " + getTxStatus(transactionId));
     }
-    
+
+    /**
+     * Return the status of the transaction with the ID given
+     * @param transactionId - The id of the transaction to find status of
+     * @return the status of the transaction
+     */
     private String getTxStatus(String transactionId) {
+        // Check if transaction is in the executed list
         for (Transaction transaction : executed_list) {
             if (transaction.uniqueId.equals(transactionId)) {
                 return "Executed";
             }
         }
+
+        // Check if the transaction is in outstanding_collection 
         for (Transaction transaction : outstanding_collection) {
             if (transaction.uniqueId.equals(transactionId)) {
                 return "In queue";
             }
         }
+
         return "Not Found";
     }
     
+    /**
+     * Empty executed_list
+     */
     private void cleanHistory() {
         executed_list.clear();
     }
     
-    private List<String> memberInfo() {
+    /**
+     * Prints the members to the user
+     */
+    private void memberInfo() {
         // TODO: Should print out members in string format
-        List<String> members = new ArrayList<>();
-        return members;
+        System.out.println("Current members are: ");
     }
     
+    /**
+     * Pause execution of thread for specified seconds
+     * Only pauses when commands are retrieved from a batch file
+     * @param duration - How many seconds to sleep for
+     */
     private void sleep(int duration) {
         // If this is called while file_name is not defined (i.e user is inputing commands) no sleep will happen
         if (file_name.isEmpty()) {
@@ -363,8 +466,10 @@ public class Client {
         }
     }
     
+    /**
+     * Disconnects from spread and exits the program
+     */
     private void exit() {
-        // Exit the client application
         try {
             spread_connection.disconnect();
         } catch (SpreadException e) {
