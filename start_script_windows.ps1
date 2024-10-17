2597229fe21cda52814fe95bc47349adddbc23cf
Kevin N. Mathisen <kevin.nikolai.mathisen@gmail.com>
2024-10-17 12:59:32 +0200

    Fix java command to use '' in powershell start script

--- start_script_windows.ps1
+++ start_script_windows.ps1
@@ -2,12 +2,12 @@
 $workDir = ".\"
 
 # Run Rep1
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar com.example.Client --server 127.0.0.1 --account Group10 --file Rep1.txt --replicas 2 --id Rep1"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp 'target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar' com.example.Client --server 127.0.0.1 --account Group10 --file Rep1.txt --replicas 2 --id Rep1"
 
 # Run Rep2
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar com.example.Client --server 127.0.0.1 --account Group10 --file Rep2.txt --replicas 2 --id Rep2"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp 'target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar' com.example.Client --server 127.0.0.1 --account Group10 --file Rep2.txt --replicas 2 --id Rep2"
 
 Start-Sleep -Seconds 15
 
 # Run Rep3
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar com.example.Client --server 127.0.0.1 --account Group10 --file Rep3.txt --replicas 2 --id Rep3"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $workDir; java -cp 'target/assignment2-1.0-SNAPSHOT.jar;lib\spread.jar' com.example.Client --server 127.0.0.1 --account Group10 --file Rep3.txt --replicas 2 --id Rep3"
